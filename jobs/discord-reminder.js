08e5de5f21d4adb10e32703998c337106c738dfb
llupRisinglll <luisedward.miranda@gmail.com>
2023-07-10 05:39:51 +0800

    feat: mongodb based cron job

--- jobs/discord-reminder.js
+++ jobs/discord-reminder.js
@@ -1,106 +1,214 @@
-const { Client, IntentsBitField } = require('discord.js');
-
-// load the .env file
 require('dotenv').config();
+const mongoose = require('mongoose');
 
-// set cronjob to use asia/singapore timezone
-process.env.TZ = 'Asia/Singapore';
-
-const myIntent = new IntentsBitField();
-
-myIntent.add(IntentsBitField.Flags.GuildPresences);
-myIntent.add(IntentsBitField.Flags.GuildMessages);
-myIntent.add(IntentsBitField.Flags.GuildMessageReactions);
-myIntent.add(IntentsBitField.Flags.GuildMessageTyping);
-myIntent.add(IntentsBitField.Flags.GuildVoiceStates);
-myIntent.add(IntentsBitField.Flags.GuildInvites);
-myIntent.add(IntentsBitField.Flags.GuildWebhooks);
-myIntent.add(IntentsBitField.Flags.GuildIntegrations);
-myIntent.add(IntentsBitField.Flags.GuildEmojisAndStickers);
-myIntent.add(IntentsBitField.Flags.GuildMembers);
-myIntent.add(IntentsBitField.Flags.Guilds);
+const http = require("https");
 
 
-const DiscordClient = new Client({
-    intents: myIntent
-});
+class ReminderScheduler {
+    constructor() {
+        this.cronSchedules = [];
+        this.tries = 0;
+        this.maxTries = 10;
+        this.currentTimestamp = new Date();
+        this.threeMinutesAgo = new Date(this.currentTimestamp.getTime() - 3 * 60000);
+        this.timerId = null;
 
+        // Define the Reminder schema
+        const reminderSchema = new mongoose.Schema({
+            title: {
+                type: String,
+                required: true
+            },
+            message: {
+                type: String,
+                required: true
+            },
+            timeHour: {
+                type: Number,
+                required: true
+            },
+            timeMinute: {
+                type: Number,
+                required: true
+            },
+            day: {
+                type: Number
+            },
+            server: {
+                type: String,
+                required: true
+            },
+            channel: {
+                type: String,
+                required: true
+            },
+            lastExecuted: {
+                type: Date
+            }
+        });
 
-DiscordClient.on('ready', async () => {
-    const PKDServer = '912962966062764062';
-    const channelId = '957557291207499837'; // Replace with known channel ID
-    const channel = DiscordClient.channels.cache.get(channelId);
+        // Create the Reminder model
+        this.Reminder = mongoose.model('Reminder', reminderSchema);
+    }
 
+    connectToMongoDB() {
+        return mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+    }
 
-    if (!channel) {
-        console.error('The channel does not exist!');
-        return;
+    async queryReminders() {
+        try {
+            const today = new Date();
+            const singaporeTime = new Date(today.toLocaleString('en-US', { timeZone: 'Asia/Singapore' }));
+
+            singaporeTime.setHours(0, 0, 0, 0); // Set the time to the start of today in Singapore time
+
+            const reminders = await this.Reminder.find({
+                $or: [
+                    { lastExecuted: { $exists: false } },
+                    { lastExecuted: { $lt: today } }
+                ]
+            }).exec();
+
+            this.cronSchedules = (reminders);
+
+            // Use cronSchedule variable for further processing
+            console.log('Query successful. cronSchedule:', this.cronSchedules);
+        } catch (error) {
+            console.error('Query failed:', error);
+
+            if (this.tries < this.maxTries) {
+                this.tries++;
+                setTimeout(() => this.queryReminders(), 5000);
+            }
+        }
+    }
+
+    async sendMessage(message, server, channel, callback) {
+        try {
+            const endpoint = `https://mir4-serverless.ue.r.appspot.com/api/discord/reminder/${server}/${channel}`;
+            const formData = new URLSearchParams();
+            formData.append('message', message);
+
+            const requestOptions = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+            };
+
+            const request = http.request(endpoint, requestOptions, (response) => {
+                let responseData = '';
+
+                response.on('data', (chunk) => {
+                    responseData += chunk;
+                });
+
+                response.on('end', () => {
+                    if (response.statusCode !== 200) {
+                        throw new Error('Request failed with status code ' + response.statusCode);
+                    }
+
+                    const jsonResponse = JSON.parse(responseData);
+                    if (jsonResponse.success === true) {
+                        callback(); // Invoke the provided callback function
+                    }
+                    console.log({ message: 'executed' });
+                });
+            });
+
+            request.on('error', (error) => {
+                console.error('Error:', error);
+            });
+
+            request.write(formData.toString());
+            request.end();
+        } catch (error) {
+            console.error('Error:', error);
+        }
     }
 
 
-    const guild = DiscordClient.guilds.cache.get(PKDServer);
-    const roles = guild.roles.cache;
-    
-    roles.forEach(role => {
-      console.log(role.id, role.name);
-    });
 
+    async start() {
+        await this.connectToMongoDB();
+        await this.queryReminders();
 
-    // Import the DISCORD schedules JSON
-    let cronSchedules;
-    let tries = 0;
-    const maxTries = 10;
-    
-    while (tries < maxTries) {
-      try {
-        cronSchedules = require('../jobs/discord-reminder.config.json');
-        break;
-      } catch (err) {
-        tries++;
-        console.error(`Error parsing JSON file, attempt ${tries}/${maxTries}:`, err);
-      }
-    }
-    
-    if (!cronSchedules) {
-      console.error(`Unable to parse JSON file after ${maxTries} attempts, exiting...`);
-      process.exit(1);
-    }
+        // Get the updated data
+        this.timerId = setInterval(async () => {
+            await this.queryReminders();
+        }, 1000 * 60 * 3);
 
-    // interval every 5 minutes
-    setInterval(async () => {
-        // check the current time in utc+8
         const now = new Date();
-        const utc8 = now.toLocaleString('en-US', { timeZone: 'Asia/Singapore' });
-        const utc8Date = new Date(utc8);
-        const utc8Hour = utc8Date.getHours();
-        const utc8Minute = utc8Date.getMinutes();
+        const next3Minutes = new Date(
+            now.getFullYear(),
+            now.getMonth(),
+            now.getDate(),
+            now.getHours(),
+            now.getMinutes() + 3,
+            0
+        );
 
-        console.log("Checking jobs for the time", utc8Hour, utc8Minute);
-        
-        // loop through the config but limit the ram usage
-        for (let i = 0; i < cronSchedules.length; i++) {
-            const { timeHour, timeMinute, message, day } = cronSchedules[i];
+        setInterval(() => {
+            const now = new Date();
+            const utc8 = now.toLocaleString('en-US', { timeZone: 'Asia/Singapore' });
+            const utc8Date = new Date(utc8);
+            const utc8Hour = utc8Date.getHours();
+            const utc8Minute = utc8Date.getMinutes();
 
-            if (utc8Hour === timeHour && utc8Minute === timeMinute) {
+            console.log('Checking jobs for the time', utc8Hour, utc8Minute);
 
+            for (let i = 0; i < this.cronSchedules.length; i++) {
+                const { timeHour, timeMinute, message, server, channel, day, _id, lastExecuted } = this.cronSchedules[i];
 
-                // if day is defined, check if the day is the same as the current day before sending the message
-                if (typeof day !== 'undefined' && day !== null && day < 7 && day >= 1) 
-                {
-                    const utc8Day = utc8Date.getDay() + 1;  // 0 is sunday, 1 is monday, 2 is tuesday, 3 is wednesday, 4 is thursday, 5 is friday, 6 is saturday
-                    
-                    // check if the current utc day matched day in config
-                    if (utc8Day !== day) {
+                // Stop executing if we already excuted it.
+                if (lastExecuted) {
+                    const lastExecutedDate = new Date(lastExecuted);
+                    const lastExecutedHour = lastExecutedDate.getHours();
+                    const lastExecutedMinute = lastExecutedDate.getMinutes();
+
+                    if (
+                        lastExecutedDate.getDate() === utc8Date.getDate() &&
+                        lastExecutedHour === utc8Hour &&
+                        lastExecutedMinute === utc8Minute
+                    ) {
                         continue;
                     }
                 }
 
-                channel.send(message);
-                console.log("Sent message", message);
-            }
-        }
-    }, 1000 * 60 * 1); // should be 1 minutes
-});
+                if (utc8Hour === timeHour && utc8Minute === timeMinute) {
+                    if (typeof day !== 'undefined' && day !== null && day < 7 && day >= 1) {
+                        const utc8Day = utc8Date.getDay() + 1;
+                        if (utc8Day !== day) {
+                            continue;
+                        }
+                    }
 
-// Log our bot in using the token from https://discord.com/developers/applications
-DiscordClient.login(process.env.DISCORD_TOKEN);
+                    this.sendMessage(message, server, channel,  () => {
+                        // Update the MongoDB document with matching _id and set lastExecuted to current datetime
+                        this.Reminder.updateOne(
+                            { _id },
+                            { $set: { lastExecuted: new Date() } },
+                            { upsert: false }
+                        )
+                            .then(updateResult => {
+                                console.log('Reminder updated successfully.');
+                                // Update the data in this.cronSchedules array
+                                this.cronSchedules[i].lastExecuted = new Date();
+                            })
+                            .catch(error => {
+                                console.error('Error updating reminder:', error);
+                            });
+                    });
+                }
+            }
+        }, 1000 * 10);
+
+
+    }
+}
+
+const reminderScheduler = new ReminderScheduler();
+reminderScheduler.start();
